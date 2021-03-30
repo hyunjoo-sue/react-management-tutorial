0face61cd91b836bc8e6cb147ef2b0ad5d94510c
suelee <sue@ecoportal.co.nz>
2021-03-30 23:51:19 +1300

    first

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Customer from './components/Customer'
+
+const customers = [{
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/any',
+  'name' : '이현주',
+  'birthday' : '961222',
+  'gender' : '여자',
+  'job' : '학생'
+},
+{
+  'id' : 2,
+  'image' : 'https://placeimg.com/64/64/any',
+  'name' : '이현주',
+  'birthday' : '961222',
+  'gender' : '여자',
+  'job' : '학생2'
+},
+{
+  'id' : 3,
+  'image' : 'https://placeimg.com/64/64/any',
+  'name' : '이현주3',
+  'birthday' : '961222',
+  'gender' : '여자',
+  'job' : '학생3'
+}
+]
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"/>
-      <h2>Let's develop management system</h2>
-
-    </div>
+    <div>
+      {
+        customers.map(customer => {
+          return(
+            <Customer
+            key={customer.id}
+            id= {customer.id}
+            img= {customer.image}
+            name = {customer.name}
+            birthday={customer.birthday}
+            gender={customer.gender}
+            job={customer.job}
+          />
+          )
+        })
+      }
+  </div>
   );
 }
 
